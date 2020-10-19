c1b85b3ffbe9d4e8f10675b9208026403ff2f020
ezwelty <ethan.welty@gmail.com>
2020-10-19 13:40:55 -0600

    Version control dev dependencies in nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,39 @@
+import tempfile
+
 import nox
 
 nox.options.sessions = "lint", "test"
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8"])
 def test(session):
     args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
     session.run("pytest", *args)
 
 
 @nox.session(python="3.8")
 def lint(session):
-    args = session.posargs or ("src", "tests", "noxfile.py")
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    args = session.posargs or locations
+    install_with_constraints(session, "flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def format(session):
-    args = session.posargs or ("src", "tests", "noxfile.py")
-    session.install("black")
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
     session.run("black", *args)
