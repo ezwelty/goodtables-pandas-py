0e14573169b6c55bd04394fbc8e10227eac2d000
ezwelty <ethan.welty@gmail.com>
2020-10-19 13:29:12 -0600

    Lint import order with flake8-import-order

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ def test(session):
 @nox.session(python="3.8")
 def lint(session):
     args = session.posargs or ("src", "tests", "noxfile.py")
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
